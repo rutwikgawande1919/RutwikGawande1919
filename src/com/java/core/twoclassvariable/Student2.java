1a110b4be3dadab1d3c59322c486733b1be10591
rutwikgawande1919 <rutwikgawande123@gmail.com>
2026-04-27 16:24:24 +0530

    practice on declared variable

---
+++ src/com/java/core/twoclassvariable/Student2.java
@@ -0,0 +1,8 @@
+package com.java.core.twoclassvariable;
+
+public class Student2 {
+
+	int id;
+	String name;
+	String city;
+}
---
+++ src/com/java/core/twoclassvariable/Test2.java
@@ -0,0 +1,19 @@
+package com.java.core.twoclassvariable;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+
+		Student2 sc = new Student2();
+		
+		sc.id = 101;
+		sc.name = "Rutwik";
+		sc.city = "pune";
+		
+		System.out.println(sc.id);
+		System.out.println(sc.name);
+		System.out.println(sc.city);
+		
+	}
+
+}
