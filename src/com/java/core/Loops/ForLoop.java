2303e9b5e58d65f1b2ce1b8496d7a5f9728b4447
rutwikgawande1919 <rutwikgawande123@gmail.com>
2026-05-02 20:37:26 +0530

    For loop and Do While loop implemented

--- src/com/java/core/Loops/ForLoop.java
+++ src/com/java/core/Loops/ForLoop.java
@@ -5,15 +5,34 @@ import java.util.Scanner;
 public class ForLoop {
 
 	public static void main(String[] args) {
-     
-		Scanner input = new Scanner(System.in);
-		System.out.println("Enter a input");
-		 int number = input.nextInt();
-		
+ /*When we write the for loop the outer execution,
+  and the inner execution will be little bit different.
+      
+      *when we have to write the program 
+      we will write on a screen it is called as outer execution.
+      eg-:
+      *1-:Declare + initialization
+      *2-:Condition/Statement 
+      *3-:Increment/Decrement
+      *4-:print statement 
+      *And we have to write the first three stage 
+      in the parenthesis of for loop and print the statement
+      in the for loop body. 
+      */
+
 		for(int i = 1 ; i <= 100 ; i++) {
 			System.out.println( +i);
-			
 		}
+/*But when we run the code the code running process is 
+ called as inner execution,and the sequence of inner execution 
+ is little bit different from outer execution
+ eg-:
+      *1-:Declare + initialization
+      *2-:Condition/Statement 
+      *3-:print statement 
+      *4-:Increment/Decrement
+ */
+		
 	}
 
 }
---
+++ src/com/java/core/Loops/WhileLoop.java
@@ -0,0 +1,26 @@
+package com.java.core.Loops;
+
+public class WhileLoop {
+
+	public static void main(String[] args) {
+
+	/* When we write a code with the help of 
+	  WHILE LOOP the outer execution and inner execution 
+	  will be same sequence like,
+	  *1-: Declare + initialization.
+	  *2-: condition/Statement.
+	  *3-: Print inner body of while loop.
+	  *4-:Increment/Decrement. */
+		
+//1-:initialization
+		int a =1;
+//condition/Statement,given in parenthesis
+		while(a<=5) {
+//print			
+			System.out.println(a);
+//Increment/Decrement			
+			a++;
+		}
+	}
+
+}
